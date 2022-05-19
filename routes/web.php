cf8fda036492eedc077316c640ed63fabd770b4b
Shoogstad <shoogstad@icloud.com>
2022-05-19 13:24:54 +0200

    Revert "Merge branch 'development' into feature-cart-blade"
    This reverts commit 4855ef9f713a38c95ea45025cce77f21bea98404.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('open.homepage');
+    return view('layouts.layout');
 });
 
 Route::get('/cart', function () {
