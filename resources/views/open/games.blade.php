a4a11ed71bd2b351dc314c6ffc4a3d483c1d3f1c
Rody <rody.van.dijk@gmail.com>
2022-05-19 13:42:42 +0200

    Gamespage thingy 1

---
+++ resources/views/open/games.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.layout')
+
+@section('topmenu')
+    <nav class="card bg-white">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="relative flex items-center justify-between h-16">
+                <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="sm:block sm:ml-6">
+                        <div class="flex space-x-20">
+                            <a href="" class="text-gray-800 px-3 py-2 rounded-md text-sm font-medium">Overzicht Games</a>
+                            <a href="" class="text-gray-800 px-3 py-2 rounded-md text-sm font-medium">Game Toevoegen</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+@endsection
+
+@section('content')
+
+    <div class="card mt-6">
+        <!--header-->
+        <div class="card-header flex flex-row justify-between">
+            <h1 class="h6">Games</h1>
+        </div>
+        <!--end header-->
+
+    </div>
+    <table class="w-full whitespace-no-wrap">
+        <thead>
+        <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b bg-gray-50">
+            <th class="px-4 py-3">Game</th>
+            <th class="px-4 py-3">Description</th>
+            <th class="px-4 py-3">Category</th>
+            <th class="px-4 py-3">Price</th>
+        </tr>
+        </thead>
+        <tbody class="bg-white divide-y">
+{{--        @foreach( as $item)--}}
+            <tr class="text-gray-700">
+                <td class="px-4 py-3 text-sm"></td>
+                <td class="px-4 py-3 text-sm"></td>
+                <td class="px-4 py-3 text-sm"></td>
+                <td class="px-4 py-3 text-sm"></td>
+            </tr>
+{{--        @endforeach--}}
+        </tbody>
+    </table>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.layout');
 });
+
+Route::get('/games', function () {
+    return view('open.games');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
