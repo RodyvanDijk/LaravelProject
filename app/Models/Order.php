550dfcacb20f0b5ffe6b99789ab22c626889633a
Dimitri van Velde <9016367@student.tcrmbo.nl>
2022-05-20 13:57:23 +0200

    Things Created

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    //relation to OrderRow
+    public function orderrows()
+    {
+        return $this->hasMany(OrderRow::class);
+    }
+
+    //relation to User
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
 }
