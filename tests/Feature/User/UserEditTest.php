c4bb51c063e597f40a2323458eb9531c757ce7b8
Dimitri van Velde <9016367@student.tcrmbo.nl>
2022-06-07 12:26:40 +0200

    User Feature tests

--- tests/Feature/User/UserEditTest.php
+++ tests/Feature/User/UserEditTest.php
@@ -8,42 +8,36 @@ beforeEach(function (){
     $this->seed('RoleAndPermissionSeeder');
     $this->seed('UserSeeder');
 
-    $this->category = Category::factory()->create();
     $this->user = User::factory()->create();
 });
 
-test('admin can see the users edit page', function () {
+test('admin can see the user edit page', function () {
+    $this->withoutExceptionHandling();
+
     $admin = User::find(3);
     Laravel\be($admin)
-        ->get(route('users.edit', ['user' => $this->user->id]))
+        ->get(route('admin.users.edit', ['user' => $this->user->id]))
         ->assertViewIs('admin.users.edit')
-        ->assertSee($this->user->user)
-        ->assertSee($this->user->description)
-        ->assertSee($this->user->category->name)
-        ->assertSee(round($this->user->price))
+        ->assertSee($this->user->name)
+        ->assertSee($this->user->email)
         ->assertStatus(200);
 })->group('User', 'UserEdit');
 
-test('salesperson can see the users edit page', function () {
+test('salesperson can not see the user edit page', function () {
     $salesperson = User::find(2);
     Laravel\be($salesperson)
-        ->get(route('users.edit', ['user' => $this->user->id]))
-        ->assertViewIs('admin.users.edit')
-        ->assertSee($this->user->user)
-        ->assertSee($this->user->description)
-        ->assertSee($this->user->category->name)
-        ->assertSee(round($this->user->price))
-        ->assertStatus(200);
-})->group('User', 'UserEdit');
-
-test('user can not see the users edit page', function () {
-    $user = User::find(1);
-    Laravel\be($user)
-        ->get(route('users.edit', ['user' => $this->user->id]))
+        ->get(route('admin.users.edit', ['user' => $this->user->id]))
         ->assertStatus(403);
 })->group('User', 'UserEdit');
 
-test('guest can not see the users edit page', function () {
-    $this->get(route('users.edit', ['user' => $this->user->id]))
+test('user can not see the user edit page', function () {
+    $user = User::find(1);
+    Laravel\be($user)
+        ->get(route('admin.users.edit', ['user' => $this->user->id]))
+        ->assertStatus(403);
+})->group('User', 'UserEdit');
+
+test('guest can not see the user edit page', function () {
+    $this->get(route('admin.users.edit', ['user' => $this->user->id]))
         ->assertStatus(403);
 })->group('User', 'UserEdit');
